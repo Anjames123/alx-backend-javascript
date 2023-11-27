ec2b81348c663771c57d619f313bd21b863417c6
Anjames123 <ajibadejames19@gmail.com>
2023-11-27 20:52:58 +0000

    tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    task = false;  // Removed "var" here
+    task2 = true; // Removed "var" here
   }
 
   return [task, task2];
