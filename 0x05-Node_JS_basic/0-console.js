ffd44ba69028bc49de9985c72853846d2319ca23
Ajibade Aanuoluwa James <ajibadejames19@gmail.com>
2024-01-31 01:26:58 +0100

    0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,1 +1,11 @@
-ndn `
+/**
+  * Task 0. Display a message on the SDTOUT
+  * @param {string} message - The message to display
+  * @author Israel Kouassi <https://github/8srael>
+*/
+
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
